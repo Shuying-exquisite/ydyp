3f9c9b27a1df83a63f7c90793a2229ed73076ebf
ShuyinYang <103890056+Shuying-exquisite@users.noreply.github.com>
2024-06-26 22:02:20 +0800

    Update Operationdocument.py

--- Operationdocument.py
+++ Operationdocument.py
@@ -1,4 +1,19 @@
 import streamlit as st
+from markdown import markdown
+from bs4 import BeautifulSoup
+
+# 读取 Markdown 文件
 with open("Operationdocument.md", "r", encoding="utf-8") as file:
     markdown_text = file.read()
-st.markdown(markdown_text)
+
+# 使用 BeautifulSoup 解析 Markdown 文件中的图片引用
+html = markdown(markdown_text)
+soup = BeautifulSoup(html, 'html.parser')
+
+# 显示 Markdown 文件中的文字内容
+st.markdown(markdown_text, unsafe_allow_html=True)
+
+# 显示 Markdown 文件中的图片
+for img in soup.find_all('img'):
+    img_url = img['src']
+    st.image(img_url)
